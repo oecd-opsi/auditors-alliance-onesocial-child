561b4b75dd63c70e6135c6c73567878fab7f8441
conlaccento <antonio@blackstudio.it>
2020-09-24 16:25:38 +0200

    Handle Function - Other option required in Registration form

--- js/bs-script.js
+++ js/bs-script.js
@@ -3,5 +3,18 @@
 
     $('input[type="password"]').hidePassword(true);
 
+    // Handle Function - Other option required in Registration form
+    if($('#signup_form')) {
+      $('#field_287 input').on( 'change', function(){
+        if( $('#option_383').is(':checked') ) {
+          $('.field_367').addClass('required-field').removeClass('optional-field');
+          $('#field_367').attr('required', 'true').attr('aria-required', 'true');
+        } else {
+          $('.field_367').removeClass('required-field').addClass('optional-field');
+          $('#field_367').removeAttr('required').attr('aria-required', 'false');
+        }
+      })
+    }
+
   });
 })(jQuery);
